fac3cdd80ae973ddd9083554f2d187815d6d6f1f
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-04-27 14:35:50 +0900

    Change main.js to output filelist automaticly

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -9,30 +9,73 @@ var app = http.createServer(function (request, response) {
   let title = queryData.id;
 
   if (pathname == '/') {
-    fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-      let template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-  
-      `;
-      response.writeHead(200);
-      response.end(template);
-    });
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (error, filelist) {
+        title = 'Welcome';
+        description = 'Hello World.';
+
+        var list = `<ul>`;
+
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + `</ul>`;
+
+        let template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+
+    } else {
+      fs.readdir('./data', function (error, filelist) {
+        var list = `<ul>`;
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + `</ul>`;
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          title = queryData.id;
+          let template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+      
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+
+    }
+
   } else {
     response.writeHead(404);
     response.end('Not found');
