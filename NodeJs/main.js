5fb2384fd09d2f9cb5ae3abb7e763c7c5e4bc559
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-05-27 21:10:17 +0900

    Make update_process active

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -77,7 +77,7 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -106,6 +106,52 @@ var app = http.createServer(function (request, response) {
           Location: `/?id=${title}`,
         });
         response.end();
+      });
+    });
+  } else if (pathname === '/update') {
+    fs.readdir('./data', function (error, filelist) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+          `,
+          `<a href="./create">create</a> <a href="./update?id=${title}">update</a>`,
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      // console.log(post);
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`./data/${title}`, description, 'utf-8', function (err) {
+          response.writeHead(302, {
+            Location: `/?id=${title}`,
+          });
+          response.end();
+        });
       });
     });
   } else {
